b935ad953ae39e73a84ba6dd4def0ebfc02cc4c9
riordanalfredo <riordan.alfredo@gmail.com>
2021-08-31 14:01:05 +1000

    update root initiator files (package.json and webpack)

--- webpack.common.js
+++ webpack.common.js
@@ -3,64 +3,78 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const outputDir = "./dist";
 
 module.exports = {
-    entry: path.resolve(__dirname, "src", "index.js"), //
-    output: {
-        path: path.join(__dirname, outputDir),
-        filename: "[name].js",
-        publicPath: "/dist/"
-    },
-    resolve: {
-        extensions: [".js"] // if we were using React.js, we would include ".jsx"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/, // if we were using React.js, we would use \.jsx?$/
-                use: {
-                    loader: "babel-loader",
-                    options: { presets: ["env"] } // if we were using React.js, we would include "react"
-                }
+  entry: path.resolve(__dirname, "src", "index.js"), //
+  output: {
+    path: path.join(__dirname, outputDir),
+    filename: "[name].js",
+    publicPath: "/dist/",
+  },
+  resolve: {
+    extensions: [".js"], // if we were using React.js, we would include ".jsx"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // if we were using React.js, we would use \.jsx?$/
+        use: {
+          loader: "babel-loader",
+          options: { presets: ["env"] }, // if we were using React.js, we would include "react"
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it uses publicPath in webpackOptions.output
+              publicPath: "../",
+              hmr: process.env.NODE_ENV === "development",
             },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            // you can specify a publicPath here
-                            // by default it uses publicPath in webpackOptions.output
-                            publicPath: "../",
-                            hmr: process.env.NODE_ENV === "development"
-                        }
-                    },
-                    "css-loader",
-                    "postcss-loader"
-                ]
+          },
+          "css-loader",
+          "postcss-loader",
+        ],
+      },
+      {
+        test: /\.scss/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it uses publicPath in webpackOptions.output
+              publicPath: "../",
+              hmr: process.env.NODE_ENV === "development",
             },
-            {
-                test: /\.scss/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            // you can specify a publicPath here
-                            // by default it uses publicPath in webpackOptions.output
-                            publicPath: "../",
-                            hmr: process.env.NODE_ENV === "development"
-                        }
-                    },
-                    "css-loader",
-                    "sass-loader",
-                    "postcss-loader"
-                ]
-            }
-        ]
-    },
-    plugins: [new MiniCssExtractPlugin({
-        // Options similar to the same options in webpackOptions.output
-        // all options are optional
-        filename: "[name].css",
-        chunkFilename: "[id].css",
-        ignoreOrder: false // Enable to remove warnings about conflicting order
-    }), require("autoprefixer")]
+          },
+          "css-loader",
+          "sass-loader",
+          "postcss-loader",
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "svg-url-loader",
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+      ignoreOrder: false, // Enable to remove warnings about conflicting order
+    }),
+    require("autoprefixer"),
+  ],
 };
